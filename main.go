16b4563eda5458a8c3c21ec2a912ecb28096869b
StephenPnkv <stephenpnkv@gmail.com>
2020-08-26 12:10:03 -0700

    Finished API, implemented CRUD ops, tested endpoints w/ Postman

--- main.go
+++ main.go
@@ -1,90 +1,226 @@
+//Stephen Penkov 8/26/20
+//A basic REST API connected to a mongodb database that handles CRUD operations.
+//Tested using Postman
+
 package main
 
 
 import (
 	"fmt"
-	//"log"
+	"time"
+	"log"
+	"context"
+	//"os"
 	"net/http"
 	"github.com/gorilla/mux"
 	"encoding/json"
 	"strconv"
+	"github.com/subosito/gotenv"
+	//"github.com/lib/pq"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+	//"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Book struct {
-	ID int `json:id`
-	Title string `json:title`
-	Author string `json:author`
-	Year string `json:year`
+	ID int 					`bson:"id,omitempty" json:"id"`
+	Title string 			`bson:"title,omitempty" json:"title"`
+	Author string 			`bson:"author,omitempty" json:"author"`
+	Year string 			`bson:"year,omitempty" json:"year"`
 }
 
 var books []Book 
 
-func main(){
-
-	books = append(books,Book{ID: 1, Title: "Pointers", Author: "Mr. Pointer", Year: "2012"},
-		Book{ID: 2, Title: "Recursion", Author: "Mr. Recursion", Year: "2019"},
-		Book{ID: 3, Title: "Concurrency", Author: "Mr. Concurrency", Year: "2014"},
-		Book{ID: 4, Title: "Go: Data Structures & Algorithms", Author: "Mr. Data", Year: "2015"},
-		Book{ID: 5, Title: "Hacking with Go", Author: "Mr. Hacker", Year: "2019"},
-	)
-
-	router := mux.NewRouter() 
-	router.HandleFunc("/books", getBooks).Methods("GET")
-	router.HandleFunc("/books", addBook).Methods("POST")
-	router.HandleFunc("/books",updateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
-
-	port := fmt.Sprintf(":%d",8000)
-	http.ListenAndServe(port,router)
-}
-
-func getBooks(w http.ResponseWriter, r *http.Request){
-	json.NewEncoder(w).Encode(books)
-}
-
-func addBook(w http.ResponseWriter, r *http.Request){
-	var book Book 
-	_ = json.NewDecoder(r.Body).Decode(&book)
-
-	books = append(books, book)
-
-	json.NewEncoder(w).Encode(books)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request){
-	params := mux.Vars(r)
-	i, _ := strconv.Atoi(params["id"])
-
-	for _, book := range books{
-		if book.ID == i{
-			json.NewEncoder(w).Encode(&book)
-		}
+func logFatal(err error){
+	if err != nil{
+		log.Fatal(err)
 	}
 }
 
-func updateBook(w http.ResponseWriter, r *http.Request){
-	var book Book 
 
-	json.NewDecoder(r.Body).Decode(&book)
-	for i, item := range books{
-		if item.ID == book.ID{
-			books[i] = book
-		}
-	} 
-	json.NewEncoder(w).Encode(books)
+func init(){
+	gotenv.Load()
 }
 
-func removeBook(w http.ResponseWriter, r *http.Request){
-	params := mux.Vars(r) 
+
+func getMongoDBConnection()(*mongo.Client,error){
+	//Helper function connects a client to mongodb, returns 
+	//client and error.
+
+	//Connect to client
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	
+	if err != nil{
+		log.Fatal(err)
+	}
+	//Test connection
+	err = client.Ping(context.TODO(),nil)
+	if err != nil{
+		log.Fatal(err)
+	}
+	//Return client and err
+	return client, err 
+}
+
+func getMongoDBCollection(DBName , CollectionName string)(*mongo.Collection, error){
+	//Function establishes a connection to mongodb and returns the collection and error.
+
+	client, err := getMongoDBConnection()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	collection := client.Database(DBName).Collection(CollectionName)
+	return collection, err 
+}
+
+func main(){
+
+	fmt.Println("Starting server. . .")
+	//Create router instance
+	router := mux.NewRouter() 
+
+	//Create routes
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books/{id}",updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	//Create server and bind to port
+	port := fmt.Sprintf(":%d",8000)
+	fmt.Printf("Listening on port %s", port)
+	http.ListenAndServe(port,router)
+}
+
+func getBooks(res http.ResponseWriter, req *http.Request){
+	res.Header().Add("content-type", "application/json")
+	collection, err := getMongoDBCollection("BookList","Books")
+	var allBooks []Book 
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.TODO(),10*time.Second)
+	defer cancel()
+	cursor, e := collection.Find(ctx,bson.M{})
+	if e != nil{
+		res.WriteHeader(http.StatusInternalServerError)
+		res.Write([]byte(`{"message": "` + err.Error() + `"}` ))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx){
+		var book Book 
+		cursor.Decode(&book)
+		allBooks = append(allBooks,book)
+	}
+	if err = cursor.Err(); err != nil{
+		res.WriteHeader(http.StatusInternalServerError)
+		res.Write([]byte(`{"message": "` + err.Error() + `"}` ))
+		return
+	}
+	json.NewEncoder(res).Encode(allBooks)
+}
+
+func addBook(res http.ResponseWriter, req *http.Request){
+	res.Header().Add("content-type", "application/json")
+
+	var book Book 
+	json.NewDecoder(req.Body).Decode(&book)
+	fmt.Printf("ID: %d | title: %s | author: %s | year: %s\n", book.ID, book.Title, book.Author, book.Year)
+
+	collection, err := getMongoDBCollection("BookList","Books")
+	if err != nil{
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, _ := collection.InsertOne(ctx,book)
+	json.NewEncoder(res).Encode(result)
+}
+
+func getBook(res http.ResponseWriter, req *http.Request){
+	
+	res.Header().Add("content-type", "application/json")
+	params := mux.Vars(req)
+	var book Book
+	
 	id, _ := strconv.Atoi(params["id"])
 
-	for i, item := range books{
-		if item.ID == id{
-			books = append(books[:i], books[i+1:]...)
-		}
-	} 
-	json.NewEncoder(w).Encode(books)
+	filter := bson.D{primitive.E{Key: "id", Value: id}}
+	collection, err := getMongoDBCollection("BookList","Books")
+	if err != nil{
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = collection.FindOne(ctx,filter).Decode(&book)
+	if err != nil{
+		res.WriteHeader(http.StatusInternalServerError)
+		res.Write([]byte(`{"message": "` + err.Error() + `"}` ))
+		return
+	}
+	json.NewEncoder(res).Encode(&book)
+}
+
+func updateBook(res http.ResponseWriter, req *http.Request){
+	
+	res.Header().Add("content-type", "application/json")
+	var book Book
+	
+	json.NewDecoder(req.Body).Decode(&book)
+	params := mux.Vars(req)
+	id, _ := strconv.Atoi(params["id"])
+
+	filter := bson.M{"id": id}
+	updatedBook := bson.D{
+			{"$set",bson.D{
+				{"id",id},
+				{"title",book.Title},
+				{"author",book.Author},
+				{"year",book.Year},
+			}},
+	}
+	
+	collection, err := getMongoDBCollection("BookList","Books")
+	if err != nil{
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, e := collection.UpdateOne(
+		ctx,
+		filter,
+		updatedBook,
+	)
+	if e != nil{
+		res.WriteHeader(http.StatusInternalServerError)
+		res.Write([]byte(`{"message": "` + err.Error() + `"}` ))
+		return
+	}
+
+	json.NewEncoder(res).Encode(&result)
+}
+
+func removeBook(res http.ResponseWriter, req *http.Request){
+	params := mux.Vars(req) 
+	id, _ := strconv.Atoi(params["id"])
+	filter := bson.D{{"id", id}}
+
+	collection, err := getMongoDBCollection("BookList","Books")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.TODO(),10*time.Second)
+	defer cancel()
+	result, e := collection.DeleteOne(ctx,filter)
+	if e != nil {
+		log.Fatal(e)
+	}
+	json.NewEncoder(res).Encode(&result)
 }
 
 
